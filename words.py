dbf6e4a36bbf1485a5b78462cb75897276e9b7d6
TheLostProgrammer <77237673+TheLostProgrammer@users.noreply.github.com>
2021-02-03 10:18:53 +1030

    Adding a python file with a list of words.
    This is the list that will be used to generate a word if single player is selected.

---
+++ words.py
@@ -0,0 +1,5 @@
+words = [
+    "frog", "couch", "flower", "phone", "bus", "sky", "bird", "school", "tree", "paddock", "home", "pencil",
+    "pen", "laptop", "shoe", "car", "bird", "dog", "exponent", "maths", "multiply", "happy", "power", "television",
+    "friends", "air"
+]
