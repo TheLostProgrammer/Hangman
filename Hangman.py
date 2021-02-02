7c5dd5149e1f697dfb511225e5ff2187cd5553b7
TheLostProgrammer <77237673+TheLostProgrammer@users.noreply.github.com>
2021-02-03 09:55:51 +1030

    Update to the hangman game.
    We have added support for single player as some may want to play alone. We have also fixed some bugs and changed a few of the graphics (slightly).

--- Hangman.py
+++ Hangman.py
@@ -1,14 +1,16 @@
 from tkinter import *
 from PIL import ImageTk, Image
+from random import *
+from words import words
 
 window = Tk()
 
-title = Label(window, text="Enter a word for your opponent to guess:", font=("Helvetica", 14, "bold", "italic"))
+title = Label(window, text="Enter one word for your opponent to guess:", font=("Helvetica", 14, "bold", "italic"))
 title.grid(row=0, column=1)
 wordInput = Entry(window, width=50)
-wordInput.grid(row=1, column=1)
+wordInput.grid(row=1, column=1, ipady=3)
 
-letters_title = Label(window, text="Wrong Letters", font=("Helvetica", 20, "bold", "underline"), foreground="grey")
+letters_title = Label(window, text="Used Letters", font=("Helvetica", 20, "bold", "underline"), foreground="grey")
 letters_title.grid(row=3, column=1)
 
 start_image = Image.open("images/stage0.png")
@@ -26,7 +28,7 @@ letters.grid(row=4, column=1)
 
 playerText = Label(window, text="Enter a letter:", font=("Helvetica", 14, "bold", "italic")).grid(row=7, column=1)
 player = Entry(window, width=50)
-player.grid(row=8, column=1)
+player.grid(row=8, column=1, ipady=3)
 
 
 def scaffold_interface(total):
@@ -134,26 +136,46 @@ buttonWidth = 10
 used_letters = []
 count = -1
 
+
 def get_word():
     global word
     global reveal
-    word = wordInput.get().lower()
-    if len(word) > 14:
-        title.config(text="The word was longer than 14 characters. Enter a new word:")
+    if button_state:
+        word = choice(words)
     else:
+        word = wordInput.get().lower()
+        if len(word) > 14:
+            title.config(text="The word was longer than 14 characters. Enter a new word:")
         title.config(text="Enter a word for your opponent to guess:")
     reveal = list(len(word) * "_")
-    print(word)
     wordInput.delete(0, END)
     letters.config(text="")
     used_letters.clear()
+    print(word)
+    word_display.config(text=reveal, font=("Helvetica", 100))
     window.update()
 
 
+button_state = False
+
+
+def switch():
+    global button_state
+    if button_state:
+        button_state = False
+
+        print(button_state)
+    else:
+        button_state = True
+        wordInput.config(state='disabled')
+
+        print(button_state)
 
 
 def appearance():
     global count
+    global button_state
+    print(word)
     letter = player.get().lower()
     for i in range(len(word)):
         if letter == word[i]:
@@ -174,10 +196,13 @@ def appearance():
             used_letters.append(letter)
 
         result = "".join(letter for letter in used_letters)
-        letters.config(text="\n".join([result[i:i + 26].upper() for i in range(0, len(result), 26)]), font=("Helvetica", 20))
-        letters_title.config(text=f"Wrong Letters ({len(result)})", foreground="grey")
+        letters.config(text="\n".join([result[i:i + 26].upper() for i in range(0, len(result), 26)]),
+                       font=("Helvetica", 20))
+        letters_title.config(text=f"Used Letters ({len(result)})", foreground="grey")
         print("The length is: " + str(len(result)))
         if "".join(reveal) == word:
+            button_state = False
+            wordInput.config(state='normal')
             letters_title.config(text="")
             used_letters.clear()
             letters.config(text="")
@@ -187,8 +212,11 @@ def appearance():
             panel = Label(window, image=img)
             panel.image = img
             panel.grid(row=5, column=1)
-            word_display.config(text="You guessed the correct word. Game Over!\nPress \"New Game\" to play again.", font=("Helvetica", 20))
+            word_display.config(text="You guessed the correct word. "
+                                     "Game Over!\nPress \"New Game\" to play again.", font=("Helvetica", 20))
         if count > 13:
+            button_state = False
+            wordInput.config(state='normal')
             count = -1
             letters_title.config(text="")
             used_letters.clear()
@@ -199,10 +227,8 @@ def appearance():
             panel = Label(window, image=img)
             panel.image = img
             panel.grid(row=5, column=1)
-            word_display.config(text=f"You ran out of guesses. The word was \"{word}\"\nPress \"New Game\" to play again.",
-                                font=("Helvetica", 20))
-
-
+            word_display.config(text=f"You ran out of guesses. The word was "
+                                     f"\"{word}\"\nPress \"New Game\" to play again.", font=("Helvetica", 20))
     player.delete(0, END)
 
 
@@ -212,7 +238,9 @@ def close():
 
 def newgame():
     global count
+    global button_state
     count = -1
+    wordInput.config(state='normal')
     letters_title.config(text="")
     used_letters.clear()
     wordInput.delete(0, END)
@@ -226,12 +254,14 @@ def newgame():
     panel.image = img
     panel.grid(row=5, column=1)
     window.update()
+    button_state = False
 
 
-getwordButton = Button(window, text="Enter", command=get_word, width=10).grid(row=2, column=1)
-quitButton = Button(window, text="Quit", width=buttonWidth, command=close).grid(row=1, column=2)
-newgameButton = Button(window, text="New Game", width=buttonWidth, command=newgame).grid(row=1, column=0)
-appearanceButton = Button(window, text="Enter", command=appearance, width=10).grid(row=9, column=1)
+getwordButton = Button(window, text="Enter", command=get_word, width=10).grid(row=2, column=1, ipady=3)
+single_player_button = Button(window, text="Single Player", command=switch, width=10).grid(row=2, column=2, ipady=3)
+quitButton = Button(window, text="Quit", width=buttonWidth, command=close).grid(row=1, column=2, ipady=3)
+newgameButton = Button(window, text="New Game", width=buttonWidth, command=newgame).grid(row=1, column=0, ipady=3)
+appearanceButton = Button(window, text="Enter", command=appearance, width=10).grid(row=9, column=1, ipady=3)
 
 
 window.mainloop()
